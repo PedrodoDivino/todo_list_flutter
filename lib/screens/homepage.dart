a0af32a2f808e3240d5ebc40d205243d586d9359
Pedro.sandrini <pedro.sandri@markdigx.com.br>
2022-06-17 19:50:42 -0300

    [Pedro] Criando TaskCard

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,24 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_app/screens/widgets.dart';
 
 class Homepage extends StatefulWidget {
   @override
   _HomepageState createState() => _HomepageState();
 }
+
 class _HomepageState extends State<Homepage> {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+      body: SafeArea(
+          child: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: 24.0,
+          vertical: 32.0,),
         child: Column(
           children: [
-           Image.asset(
- 'assets/image/logo.png',
-  scale: 5.0,
-),
+            Image.asset(
+              'assets/image/logo.png',
+              scale: 5.0,
+            ),
+            TaskCardWidget()
           ],
         ),
-      ),
+      )),
     );
   }
 }
---
+++ lib/screens/widgets.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class TaskCardWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('teste'),
+    );
+  }
+}
